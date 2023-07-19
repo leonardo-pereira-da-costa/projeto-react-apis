30e22f30a5f0a28a10c3cb9954ae88f1e47d7e98
Nhaykwollfer <108698094+Nhaykwollfer@users.noreply.github.com>
2023-07-19 17:35:46 -0300

    Repo correto para entregado Projeto Pokedex

---
+++ pokedex/src/App.js
@@ -0,0 +1,15 @@
+import ModalPokemon from './Components/modal/modal'
+import GlobalState from './Global/GlobalState'
+import Router from './Router/Router'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export default function App() {
+  return (
+    <GlobalState>
+      <ChakraProvider>
+        <ModalPokemon />
+        <Router />
+      </ChakraProvider>
+    </GlobalState>
+  )
+}
